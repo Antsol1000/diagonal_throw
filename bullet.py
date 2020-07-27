e2caa11d93b733bf7d2fbcb0a2c43fef129ba6be
Antoni Solarski <antsol1000@gmail.com>
2020-07-28 00:17:26 +0200

    added class which calculates diagonal throw insted of c++

---
+++ bullet.py
@@ -0,0 +1,74 @@
+"""
+class Bullet is used to calculate the diagonal throw of sphere
+"""
+
+# imports
+import math
+import matplotlib.pyplot as plt
+
+
+# constants
+
+# gravity acceleration
+G = 9.81
+
+# time step
+DELTA = 0.0001
+
+
+class Bullet:
+    drag_coefficient = 0.47
+    radius, mass, angle, velocity = 0, 0, 0, 0
+    x, y, v_x, v_y, a_x, a_y = 0, 0, 0, 0, 0, -G
+    time = 0
+
+    def __init__(self, radius, mass, angle, velocity):
+        self.radius = radius
+        self.mass = mass
+        self.angle = angle * math.pi / 180
+        self.velocity = velocity
+        self.air_resistance_factor = 0.5 * 1.2 * math.pi * self.radius ** 2 * self.drag_coefficient
+        self.v_x = self.velocity * math.cos(self.angle)
+        self.v_y = self.velocity * math.sin(self.angle)
+
+    def step(self):
+        self.time += DELTA
+
+        self.a_x = -self.air_resistance_factor * self.v_x * abs(self.v_x) / self.mass
+        self.a_y = -self.air_resistance_factor * self.v_y * abs(self.v_y) / self.mass - G
+
+        self.v_x += DELTA * self.a_x
+        self.v_y += DELTA * self.a_y
+
+        self.x += self.v_x * DELTA
+        self.y += self.v_y * DELTA
+
+    def get_time(self):
+        return self.time
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+
+if __name__ == '__main__':
+
+    bullet = Bullet(0.075, 0.15, 30, 42)
+    x, y = [], []
+
+    bullet.step()
+    while bullet.get_y() > 0:
+        x.append(bullet.get_x())
+        print(bullet.get_y(), bullet.v_y, bullet.a_y)
+        y.append(bullet.get_y())
+        bullet.step()
+
+    plt.plot(x, y)
+    x_min, x_max = plt.xlim()
+    y_min, y_max = plt.ylim()
+    xy_max = max(x_max, y_max)
+    plt.xlim(x_min, xy_max)
+    plt.ylim(y_min, xy_max)
+    plt.show()
